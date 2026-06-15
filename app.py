cc21d24aa21d4c471baa4eb2b2e263b11dfbcb3b
Justyn-17 <25048740@myrp.edu.sg>
2026-06-15 11:36:19 +0800

    Justyn menu feature updated

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/api/menu')
+def api_menu():
+    return {
+        "foods": [
+            {"name": "Chicken Rice", "price": 5.50},
+            {"name": "Nasi Lemak", "price": 4.50},
+            {"name": "Burger", "price": 6.00}
+        ]
+    }
+
 if __name__ == '__main__':
     app.run(debug=True)
